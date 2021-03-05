a38320154fa3ef7fe445595875c340ea2b60c20f
ngittlen <nggittle@med.umich.edu>
2021-03-05 11:51:45 -0500

    New sample spring app to be packaged

---
+++ src/main/java/com/nfjs/helloservice/HelloController.java
@@ -0,0 +1,20 @@
+package com.nfjs.helloservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final GreetingProps props;
+
+    public HelloController(GreetingProps props) {
+        this.props = props;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return props.getMessage();
+    }
+
+}
